bb45a1eb211acf6a06830d787cebdb401dce600f
Wildhevire <wildhevire404@gmail.com>
2019-09-26 17:45:00 +0000

    finished

---
+++ src/latihan/teks/Teks.java
@@ -0,0 +1,17 @@
+package latihan.teks;
+
+public class Teks {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : mendeklarasikan variabel teks
+     */
+	public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newLine = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newLine + "Char:" + tab + "berisi " + c);
+    }
+}
